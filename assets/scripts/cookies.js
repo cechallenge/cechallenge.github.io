f96a915306813056fbf721a5ce3910dce4561789
jyeon.kim <kjyeon1676@naver.com>
2024-09-27 17:43:24 +0900

    Update cookies.js

--- assets/scripts/cookies.js
+++ assets/scripts/cookies.js
@@ -5,16 +5,19 @@ function setCookie( name, value, expiredays ) {
 }
 
 function closePop() {
-    if ( document.pop_form.chkbox.checked ){
+    if ( document.pop_form0.chkbox.checked || document.pop_form1.chkbox.checked){
         setCookie( "maindiv", "done" , 1 );
     }
-    document.all['layer_popup'].style.visibility = "hidden";
+    document.all['layer_popup0'].style.visibility = "hidden";
+    document.all['layer_popup1'].style.visibility = "hidden";
 }
 
 cookiedata = document.cookie;   
 if ( cookiedata.indexOf("maindiv=done") < 0 ){     
-    document.all['layer_popup'].style.visibility = "visible";
+    document.all['layer_popup0'].style.visibility = "visible";
+    document.all['layer_popup1'].style.visibility = "visible";
 }
 else {
-    document.all['layer_popup'].style.visibility = "hidden";
+    document.all['layer_popup0'].style.visibility = "hidden";
+    document.all['layer_popup1'].style.visibility = "hidden";
 }
